d75d0f7c562dc82efae1fadd4ad7883c1e04a956
mohamed <mohamedaydi30@gmail.com>
2022-06-07 21:13:39 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { RequestsManagementComponent } from './requests-management/requests-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:DashbordComponent},
+  {path:"requestsManegement",component:RequestsManagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RequestsManagementComponent } from './requests-management/requests-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbordComponent,
+    NavbarComponent,
+    RequestsManagementComponent
   ],
   imports: [
     BrowserModule,
